5240125375a49209852ea865833e6b35538a84f2
Vasiliy <vasyokbelyiv@gmail.com>
2019-02-03 21:54:59 +0500

    Fix: NullPoinerException in me.nyanguymf.console.server.net.Connection class.

--- src/main/java/me/nyanguymf/console/server/net/Connection.java
+++ src/main/java/me/nyanguymf/console/server/net/Connection.java
@@ -67,7 +67,12 @@ public final class Connection {
         }
 
         /* Deauthorize current client on close. */
-        this.storage.getAuthorized().get(this).deauthorize();
+        try {
+            this.storage.getAuthorized().get(this).deauthorize();
+        } catch (NullPointerException expected) {
+            // In the case when client doesn't authorized yet.
+        }
+
         this.config.save();
 
         if (!this.in.isCancelled())
